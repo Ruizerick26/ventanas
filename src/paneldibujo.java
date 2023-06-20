0efc59a61fea7ca325976566d2cb97bee01304db
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-20 09:10:23 -0500

    ventanas de trabajo

--- src/paneldibujo.java
+++ src/paneldibujo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class paneldibujo {
+import javax.swing.*;
+import java.awt.*;
+
+public class paneldibujo extends JPanel{
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int ancho = getWidth();
+        int alto = getHeight();
+
+        g.drawLine(0,0,ancho,alto);
+        g.drawLine(0,alto,ancho,0);
+    }
 }
+
--- src/pruebapaneldibujo.java
+++ src/pruebapaneldibujo.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class pruebapaneldibujo {
+import javax.swing.*;
+
+public class pruebapaneldibujo {
+    public static void main(String[] args) {
+        //Instancia de la clase panel
+        paneldibujo panel = new paneldibujo();
+        //crear un marco de ventana
+        JFrame aplicacion = new JFrame();
+        //boton para cerrar ventana
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aplicacion.add(panel); //agregar panel a la ventana
+        aplicacion.setSize(250,250); //settear tamaño de ventana
+        aplicacion.setVisible(true); // visualizar ventana
+
+    }
 }
