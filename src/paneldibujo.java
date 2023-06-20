04afb7ee1e7fe7dcb6808a97e90fd0567802129a
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-20 09:47:30 -0500

    ultmis cambios

--- src/paneldibujo.java
+++ src/paneldibujo.java
@@ -2,14 +2,56 @@ import javax.swing.*;
 import java.awt.*;
 
 public class paneldibujo extends JPanel{
+    public String Nombre;
+    public float peso;
+    public float altura;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public String IMC(float altura, float peso){
+            float imc = (float) (peso / (Math.pow(altura,2)));
+            String imc_t= Float.toString(imc);
+        return imc_t;
+
+    }
+
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         int ancho = getWidth();
         int alto = getHeight();
 
-        g.drawLine(0,0,ancho,alto);
-        g.drawLine(0,alto,ancho,0);
+        //g.drawLine(10,10,80,10);
+        //g.drawLine(10,50,80,50);
+        //g.drawLine(10,90,80,90);
+
+        //g.drawLine(10,10,10,90);
+
+        //g.drawString("RCEE",20,20);
+        g.drawString(getNombre(),50,50);
+        g.drawString(IMC(getPeso(),getAltura()),50,70);
+
     }
 }
 
--- src/pruebapaneldibujo.java
+++ src/pruebapaneldibujo.java
@@ -1,16 +1,34 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class pruebapaneldibujo {
     public static void main(String[] args) {
+        Scanner rc=new Scanner(System.in);
+        String Nombre;
+
+        System.out.println("Ingrese su nombre");
+        Nombre=rc.nextLine();
+        System.out.println("Ingrese su altura:");
+        float altura = rc.nextFloat();
+        System.out.println("Ingrese su peso en kg:");
+        float peso = rc.nextFloat();
+
         //Instancia de la clase panel
         paneldibujo panel = new paneldibujo();
         //crear un marco de ventana
         JFrame aplicacion = new JFrame();
+        panel.setNombre(Nombre);
+        panel.setPeso(peso);
+        panel.setAltura(altura);
         //boton para cerrar ventana
         aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         aplicacion.add(panel); //agregar panel a la ventana
         aplicacion.setSize(250,250); //settear tamaño de ventana
         aplicacion.setVisible(true); // visualizar ventana
+
+
+
+
 
     }
 }
